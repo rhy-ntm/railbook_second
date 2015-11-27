41fe82d2ef3d1c083b85bce27a4ce504ddfb44f4
natsumi <kat_tun_jinkazu_love@yahoo.co.jp>
2015-11-27 13:14:33 +0900

    15_2

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,17 @@
 class Review < ActiveRecord::Base
-  belongs_to :book
+  belongs_to :boook
+  
+  # その他バリデーション
+  validate :my_validation_method
+  
+private
+  
+  # 追加バリデーション
+  def my_validation_method
+    boook_title = Boook.find_by(id: boook_id)
+    if boook_title.blank?
+      errors.add(:boook_id, "はマスタに存在しません")
+    end
+  end
+  
 end
