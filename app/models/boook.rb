9e042fa215f8139193ef9dc160ee1c129efd0cb6
natsumi <kat_tun_jinkazu_love@yahoo.co.jp>
2015-11-27 10:50:51 +0900

    validation set

--- app/models/boook.rb
+++ app/models/boook.rb
@@ -1,2 +1,5 @@
 class Boook < ActiveRecord::Base
+  validates :isbn, presence: true, uniqueness: true
+  validates :title, length: {minimum: 1, maximum: 100}
+  validates :price, numericality: {only_integer: true}
 end
