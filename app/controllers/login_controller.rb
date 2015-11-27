effdbbbe910865fa540b09092684cc39f5a4fa16
natsumi <kat_tun_jinkazu_love@yahoo.co.jp>
2015-11-27 15:06:17 +0900

    18_login_set

---
+++ app/controllers/login_controller.rb
@@ -0,0 +1,27 @@
+class LoginController < ApplicationController
+  skip_before_action :check_logined
+  def index
+  end
+
+  def auth
+    # 入力値がユーザー情報に存在するかどうか認証する
+    ## 存在すれば対象のオブジェクト
+    ## 存在しなければnil
+    usr = User.authenticate(params[:userid],params[:password])
+    if usr then
+      # セッションの初期化
+      reset_session
+      # セッションに対象のユーザーのIDを保存
+      session[:usr] = usr.id
+      # 指定のリダイレクト先へ移動
+      redirect_to params[:referer]
+    else
+      flash.now[:referer] = params[:referer]
+      @error = 'ユーザーID/パスワードが間違っています。'
+      render 'index'
+    end
+  end
+  
+  def auth
+  end
+end
